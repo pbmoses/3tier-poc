4279192f47285fb369290f1001a16eea52474004
pmo <philmoses@gmail.com>
2025-03-24 16:11:07 -0700

    Update main.tf
    adding modular changes to single file

--- TF/main.tf
+++ TF/main.tf
@@ -9,3 +9,18 @@ provider "google" {
   project = "clgcporg10-163"
   region = "us-central1"
 }
+
+resource "google_storage_bucket" "static" {
+ name          = "mongo-backup"
+ location      = "US"
+ storage_class = "STANDARD"
+
+ uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_iam_member" "member" {
+  provider = google
+  bucket   = google_storage_bucket.default.name
+  role     = "roles/storage.objectViewer"
+  member   = "allUsers"
+}
